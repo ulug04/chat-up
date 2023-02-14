0199fb9a0260ca7d208afd9dd88ee610b40ebf36
ulug04 <fayzulug7@gmail.com>
2023-02-15 00:52:37 +0500

    gvjbj

---
+++ app/src/main/java/com/example/fifthapplication/fragments/ProfileInfo.kt
@@ -0,0 +1,6 @@
+package com.example.fifthapplication.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.fifthapplication.R
+
+class ProfileInfo:Fragment (R.layout.info_profile)
--- app/src/main/java/com/example/fifthapplication/fragments/SmsFragment.kt
+++ app/src/main/java/com/example/fifthapplication/fragments/SmsFragment.kt
@@ -2,7 +2,9 @@ package com.example.fifthapplication.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fifthapplication.R
 import com.fraggjkee.smsconfirmationview.SmsConfirmationView
 
@@ -11,9 +13,15 @@ class SmsFragment:Fragment(R.layout.fragment_sms) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val view: SmsConfirmationView = view.findViewById(R.id.sms_code_view)
-        view.onChangeListener = SmsConfirmationView.OnChangeListener { code, isComplete ->
+        val button = view.findViewById<Button>(R.id.idBtnSendMessage)
 
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_smsFragment_to_profileInfo)
+
+            val view: SmsConfirmationView = view.findViewById(R.id.sms_code_view)
+            view.onChangeListener = SmsConfirmationView.OnChangeListener { code, isComplete ->
+
+            }
         }
     }
 }
