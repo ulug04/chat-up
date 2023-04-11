efaf6559f17adb011dcabb2724f850afe843d45b
ulug04 <fayzulug7@gmail.com>
2023-04-11 22:43:50 +0500

    gvjbjhdhdhhebfhjbjdfsughhjhbws

--- app/src/main/java/com/example/fifthapplication/fragments/contacts/ContactsFragment.kt
+++ app/src/main/java/com/example/fifthapplication/fragments/contacts/ContactsFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fifthapplication.R
 import com.example.fifthapplication.databinding.ContactFragmentBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.xwray.groupie.GroupieAdapter
 
 
@@ -16,34 +18,53 @@ class ContactsFragment: Fragment (R.layout.contact_fragment) {
     private var _binding: ContactFragmentBinding? = null
     private val binding get() = _binding!!
 
+    private val database = Firebase.database.reference
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val myAdapter = GroupieAdapter()
-
-        binding.rv.adapter = myAdapter // recyclerView connected with brain (adapter)!
-
-
-
-        val usersFromFirebase = listOf(
-            UserItem( userName = "Ulug", userImage = R.drawable.icon, description = "developer"),
-            UserItem(userName = "Otabek", userImage = R.drawable.wing, description = "doctor"),
-            UserItem(userName = "Ulug", userImage = R.drawable.gmail, description = "smasher"),
-            UserItem(userName = "Ulug", userImage = R.drawable.gmail, description = "smasher"),
-            UserItem(userName = "Ulug", userImage = R.drawable.gmail, description = "smasher"),
-        )
-
-
-        myAdapter.addAll(usersFromFirebase)
+//        val myAdapter = GroupieAdapter()
+//
+//        binding.rv.adapter = myAdapter // recyclerView connected with brain (adapter)!
+//
+//
+//
+//        val usersFromFirebase = listOf(
+//            UserItem( userName = "Ulug", userImage = R.drawable.icon, description = "developer"),
+//            UserItem(userName = "Otabek", userImage = R.drawable.wing, description = "doctor"),
+//            UserItem(userName = "Ulug", userImage = R.drawable.gmail, description = "smasher"),
+//            UserItem(userName = "Ulug", userImage = R.drawable.gmail, description = "smasher"),
+//            UserItem(userName = "Ulug", userImage = R.drawable.gmail, description = "smasher"),
+//        )
+//        myAdapter.addAll(usersFromFirebase)
 
 
+
+
+        val animal1 = Animal(id = 1, name = "Kenguru", legs = 2)
+        val animal2= Animal(id = 2, name = "Dog", legs = 4)
+        val animal3 = Animal(id = 3, name = "Fish", legs = 0)
+
+
+
+        writeNewUser(animal1)
+        writeNewUser(animal2)
+        writeNewUser(animal3)
 
     }
 
 
+     private fun writeNewUser(animal:Animal) {
 
+        database.child("animals").child(animal.name).setValue(animal.legs)
+    }
 
+    data class Animal(
+        val id: Int,
+        val name: String,
+        val legs: Int
+    )
 
 
 
